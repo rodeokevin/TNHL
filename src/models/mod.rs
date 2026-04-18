3304e9cc40bf18242a9b58234bbe337bf6eeab39
Kevin Qin <kevin.qinzw@gmail.com>
2026-04-18 14:19:16 -0400

    add defunct/original team abbreviations

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -15,45 +15,65 @@ pub struct TeamName {
 #[derive(Debug, Clone, Copy, PartialEq, Default, Display, EnumString, Deserialize)]
 #[strum(ascii_case_insensitive)]
 pub enum TeamAbbrev {
-    ANA,
-    ARI,
+    AFM, // Atlanta Flames (defunct)
+    ANA, // Anaheim Ducks
+    ARI, // Arizona Coyotes (defunct)
+    ATL, // Atlanta Thrasher (defunct)
     BOS,
+    BRK, // Brooklyn Americans (defunct)
     BUF,
     CAR,
     CBJ,
+    CGS, // Bay Area Seals/California Golden Seals (Defunct)
     CGY,
     CHI,
+    CLE, // Cleveland Barons (defunct)
+    CLR, // Colorado Rockies (defunct)
     COL,
     DAL,
+    DCG, // Detroit Cougars (defunct)
     DET,
+    DFL, // Detroit Falcons (defunct)
     EDM,
     FLA,
+    HAM, // Hamilton Tigers (defunct)
     HFD,
+    KCS, // Kansas City Scouts (defunct)
     LAK,
     MIN,
-    MNS,
+    MMR, // Montreal Marrons (defunct)
+    MNS, // Minnesota North Stars (defunct)
     #[default]
     MTL,
+    MWN, // Montreal Wanderers (defunct)
     NJD,
     NSH,
+    NYA, // New York Americans (defunct)
     NYI,
     NYR,
-    OAK,
+    OAK, // California/Oakland Seals (defunct)
     OTT,
     PHI,
-    PHX,
+    PHX, // Phoenix Coyotes (defunct)
+    PIR, // Pittsburgh Pirates (defunct)
     PIT,
+    QBD, // Quebec Bulldogs (defunct)
+    QUA, // Philadelphia Quakers(defunct)
+    QUE, // Quebec Nordiques (defunct)
     SEA,
-    SEN,
+    SEN, // Ottawa Senators (original)
+    SLE, // St. Louis Eagles (defunct)
     SJS,
     SMT,
     STL,
+    TAN, // Toronto Hockey Club/Toronto Arenas (defunct)
     TBL,
     TOR,
+    TSP, // Toronto St. Patricks (defunct)
     UTA,
     VAN,
     VGK,
-    WIN,
+    WIN, // Winnipeg Jets (original)
     WPG,
     WSH,
 }
